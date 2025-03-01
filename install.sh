2c88f0870fd09a44ee316b851f77deaccd3bc0bd
Nikora <zookiee@proton.me>
2025-02-28 20:22:36 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,23 +2,21 @@
 
 # Download Fonts
 echo -e "${GREEN}Downloading Cascadia Code ${ENDCOLOR}"
-sudo pacman -S ttf-cascadia-code-nerd -y | clear
+sudo pacman -S --noconfirm ttf-cascadia-code-nerd
 echo -e "${GREEN}Downloading Patched font Roboto Mono from nerd fonts library ${ENDCOLOR}"
-sudo pacman -S ttf-roboto-mono-nerd ttf-roboto-mono -y | clear
+sudo pacman -S --noconfirm ttf-roboto-mono-nerd ttf-roboto-mono
 echo -e "${GREEN}Downloading Font Awesome ${ENDCOLOR}"
-sudo pacman -S ttf-font-awesome -y | clear
+sudo pacman -S --noconfirm ttf-font-awesome
 echo -e "${GREEN}Update Fonts ${ENDCOLOR}"
-sudo fc-cache -v | clear
+sudo fc-cache -v
 echo -e "${RED} Done with setting up fonts${ENDCOLOR}"
 
 # Download Themes
 echo -e "${GREEN} Downloading tela-circle-icon-theme-dracula ${ENDCOLOR}"
-sudo pacman -S tela-circle-icon-theme-dracula -y | clear
+sudo pacman -S --noconfirm tela-circle-icon-theme-dracula
 echo -e "${GREEN} Downloading AtomOneDarkTheme from Github ${ENDCOLOR}"
-git clone https://github.com/UnnatShaneshwar/AtomOneDarkTheme.git | clear
+git clone https://github.com/UnnatShaneshwar/AtomOneDarkTheme.git
 echo -e "${GREEN} Adding to themes folder ${ENDCOLOR}"
 sudo cp -r AtomOneDarkTheme /usr/share/themes
 
 echo -e "${RED} Done Pls Reboot!!!:3${ENDCOLOR}"
-
-done
